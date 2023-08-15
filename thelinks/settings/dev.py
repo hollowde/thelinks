74d84c20dbcaaf201ed563c71d4618df1c05418c
hollowde <hollowde@Mail.uc.edu>
2023-08-15 10:30:07 -0400

    development settings development

--- thelinks/settings/dev.py
+++ thelinks/settings/dev.py
@@ -1,13 +1,13 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bv78#+n-s=9@fomlqvvz$u6qthu06zp^oc21+cqxp8vqulj1$q"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split("")
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
